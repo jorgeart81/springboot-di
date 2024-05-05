b51a855d5fff175ff349ea4da27f6c9063da2d40
Jorge Reyes <92123185+JorgeArt81@users.noreply.github.com>
2024-05-04 21:24:00 -0500

    Create ProductRepositoryImpl

---
+++ src/main/java/com/jorge/springboot/di/app/springbootdi/repositories/ProductRepositoryImpl.java
@@ -0,0 +1,28 @@
+package com.jorge.springboot.di.app.springbootdi.repositories;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.jorge.springboot.di.app.springbootdi.controllers.Product;
+
+public class ProductRepositoryImpl implements ProductRepository {
+  private List<Product> data;
+
+  public ProductRepositoryImpl() {
+    this.data = Arrays.asList(
+        new Product(1L, "Memoria corsair 32", 300L),
+        new Product(2L, "Intel core i9", 860L),
+        new Product(3L, "Teclado Razer Mini 60&", 180L),
+        new Product(4L, "Motherboard Gigabyte", 480L));
+  }
+
+  @Override
+  public List<Product> findAll() {
+    return data;
+  }
+
+  @Override
+  public Product findById(Long id) {
+    return data.stream().filter(p -> p.getId().equals(id)).findFirst().orElseThrow();
+  }
+}
---
+++ src/main/java/com/jorge/springboot/di/app/springbootdi/services/ProductServiceImpl.java
@@ -0,0 +1,29 @@
+package com.jorge.springboot.di.app.springbootdi.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.jorge.springboot.di.app.springbootdi.controllers.Product;
+import com.jorge.springboot.di.app.springbootdi.repositories.ProductRepositoryImpl;
+
+public class ProductServiceImpl implements ProductService {
+
+  private ProductRepositoryImpl repository = new ProductRepositoryImpl();
+
+  @Override
+  public List<Product> findAll() {
+    return repository.findAll().stream().map(p -> {
+      Double priceTax = p.getPrice() * 1.25d;
+      // Product newProduct = new Product(p.getId(), p.getName(),
+      // priceTax.longValue());
+      Product newProduct = (Product) p.clone();
+      newProduct.setPrice(priceTax.longValue());
+      return newProduct;
+    }).collect(Collectors.toList());
+  }
+
+  @Override
+  public Product findById(Long id) {
+    return repository.findById(id);
+  }
+}
