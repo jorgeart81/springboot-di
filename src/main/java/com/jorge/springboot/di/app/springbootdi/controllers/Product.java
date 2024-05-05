fe39161bfe632101c32bc3e5373f0e3f1437be81
Jorge Reyes <92123185+JorgeArt81@users.noreply.github.com>
2024-05-04 21:22:20 -0500

    create Product controller

--- src/main/java/com/jorge/springboot/di/app/springbootdi/controllers/Product.java
+++ src/main/java/com/jorge/springboot/di/app/springbootdi/controllers/Product.java
@@ -1,6 +1,6 @@
 package com.jorge.springboot.di.app.springbootdi.controllers;
 
-public class Product {
+public class Product implements Cloneable {
   private Long id;
   private String name;
   private Long price;
@@ -36,6 +36,15 @@ public class Product {
 
   public void setPrice(Long price) {
     this.price = price;
+  }
+
+  @Override
+  public Object clone() {
+    try {
+      return super.clone();
+    } catch (CloneNotSupportedException e) {
+      return new Product(id, name, price);
+    }
   }
 
 }
