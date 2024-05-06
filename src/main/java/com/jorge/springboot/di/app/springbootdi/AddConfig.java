cd523faeaed5b8787ff7bc37eda79a339dc671d2
Jorge Reyes <92123185+JorgeArt81@users.noreply.github.com>
2024-05-05 22:45:38 -0500

    AddConfig environment

---
+++ src/main/java/com/jorge/springboot/di/app/springbootdi/AddConfig.java
@@ -0,0 +1,10 @@
+package com.jorge.springboot.di.app.springbootdi;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:config.properties")
+public class AddConfig {
+
+}
--- src/main/java/com/jorge/springboot/di/app/springbootdi/services/ProductServiceImpl.java
+++ src/main/java/com/jorge/springboot/di/app/springbootdi/services/ProductServiceImpl.java
@@ -1,11 +1,15 @@
 package com.jorge.springboot.di.app.springbootdi.services;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.annotation.RequestScope;
 
+import com.jorge.springboot.di.app.springbootdi.AddConfig;
 import com.jorge.springboot.di.app.springbootdi.controllers.Product;
 import com.jorge.springboot.di.app.springbootdi.repositories.ProductRepository;
 
@@ -13,16 +17,23 @@ import com.jorge.springboot.di.app.springbootdi.repositories.ProductRepository;
 @RequestScope
 public class ProductServiceImpl implements ProductService {
 
+  private Environment environment;
   private ProductRepository repository;
-  
-  public ProductServiceImpl(ProductRepository repository) {
+
+  // @Value("${config.price.tax}")
+  // private Double tax;
+
+  public ProductServiceImpl(ProductRepository repository, Environment environment) {
+    this.environment = environment;
     this.repository = repository;
   }
 
   @Override
   public List<Product> findAll() {
+    Double tax = environment.getProperty("config.price.tax", Double.class);
+
     return repository.findAll().stream().map(p -> {
-      Double priceTax = p.getPrice() * 1.25d;
+      Double priceTax = p.getPrice() * tax;
       // Product newProduct = new Product(p.getId(), p.getName(),
       // priceTax.longValue());
       Product newProduct = (Product) p.clone();
